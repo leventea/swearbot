bca0e89e28568d01612520b8bc8f195401db4303
leventea <leventea@tuta.io>
2022-11-10 09:46:56 +0100

    New config fields for deploying commands

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,8 @@ dt.config();
 
 export class Config {
     token: string;
+    clientId: string;
+    devServerId: string|undefined;
 
     static getKey(key: string): string|undefined {
         return process.env[key];
@@ -18,8 +20,19 @@ export class Config {
         return k;
     }
 
+    static warnKey(key: string, msg: string): string|undefined {
+        let k = this.getKey(key);
+        if (!k)
+            console.error(`[WARN] Optional key ${key} is not set. ${msg}`);
+
+        return k;
+    }
+
+    // NOTE: writing a DSL for this would be funny but unnecessary 
     constructor() {
         this.token = Config.requireKey('TOKEN');
+        this.clientId = Config.requireKey('CLIENT_ID');
+        this.devServerId = Config.warnKey('DEV_SERVER', 'Command deployment to the development server will be unavailable.');
     }
 }
 
