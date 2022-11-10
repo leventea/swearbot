610f77f4e6de03dc052626a23a916ac215abf26e
leventea <leventea@tuta.io>
2022-11-10 10:06:41 +0100

    Basic command handling

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,33 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { Client, Events, GatewayIntentBits, Interaction } from 'discord.js';
+import { Command, getCommands } from './commands';
 import { cfg } from './config';
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+let commands: Map<string, Command> = new Map<string, Command>();
 
-client.once(Events.ClientReady, c => {
-    console.log("Bot ready");
+client.once(Events.ClientReady, async c => {
+    (await getCommands()).forEach((cmd: Command) => { 
+        commands.set(cmd.data.name, cmd);
+    });
+
+    console.log(`Loaded ${commands.keys.length} command(s).`);
+    console.log(`Bot ready.\nInvite link: https://discord.com/oauth2/authorize/?permissions=-2147481600&scope=bot&client_id=${cfg.clientId}`);
+});
+
+client.on(Events.InteractionCreate, async (i: Interaction) => {
+    if (!i.isChatInputCommand()) return; 
+ 
+    const cmd = commands.get(i.commandName);
+    if (!cmd) {
+        console.error(`No command with name ${i.commandName} found.`);
+        return;
+    }
+   
+    try {
+        await cmd.execute(i);
+    } catch (ex) {
+        console.error(ex);
+    }
 });
 
 client.login(cfg.token);
